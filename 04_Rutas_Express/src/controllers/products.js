9b08902c0e7f3528537f9c3e8b0d1ad702a5e74e
cotalora <67478427+cotalora@users.noreply.github.com>
2022-10-13 19:02:01 -0500

    Integración de formulario

--- 04_Rutas_Express/src/controllers/products.js
+++ 04_Rutas_Express/src/controllers/products.js
@@ -1,21 +1,32 @@
+const { contenedor } = require("../../data");
+
 const products = async (req, res) => {
-    res.json({ message: 'Productos' });
+    const products = await contenedor.getAll();
+    res.json(products);
 };
 
 const product = async (req, res) => {
-    res.json({ message: `Producto: ${req.params.id}` });
+    const product = await contenedor.getById(parseInt(req.params.id));
+    Object.keys(product).length > 0 ? res.json(product) : res.status(404).json({ error: 'Item not found' });
 };
 
-const addProduct = async (req, res) => {
-    res.json({ message: `Producto: ${req.params.id}` });
+const addProduct = async ({ body }, res) => {
+    const newProduct = await contenedor.save(body);
+    Object.keys(newProduct).length > 0 ? 
+        res.json({ message: 'Product has been added', data: newProduct }) : 
+        res.status(400).json({ error: 'Item not created' });
 };
 
 const updateProduct = async (req, res) => {
-    res.json({ message: `Producto: ${req.params.id}` });
+    const result = await contenedor.updateById(parseInt(req.params.id), req.body);
+    Object.keys(result).length > 0 ? 
+        res.json({ message: 'Product has been updated', data: result }) : 
+        res.status(404).json({ error: 'Item not found' });
 };
 
 const deleteProduct = async (req, res) => {
-    res.json({ message: `Producto: ${req.params.id}` });
+    const result = await contenedor.deleteById(parseInt(req.params.id));
+    result.length > 0 ? res.json({ message: 'Product has been deleted'}) : res.status(404).json({ error: 'Item not found' });
 };
 
 
