d17f383ff4cfe9329b7edab3448ead383a2cabe0
cotalora <67478427+cotalora@users.noreply.github.com>
2022-11-10 23:30:11 -0500

    Integración de dbs

--- 07_Primera_Entrega/src/controllers/products.js
+++ 07_Primera_Entrega/src/controllers/products.js
@@ -3,31 +3,31 @@ const { producto } = require("../dataProducts");
 class ProductController {
     constructor() { }
 
-    products = async (req, res) => {
+    async products (req, res) {
         const products = await producto.getAll();
         res.json(products);
     };
 
-    product = async (req, res) => {
+    async product (req, res) {
         const product = await producto.getById(parseInt(req.params.id));
         Object.keys(product).length > 0 ? res.json(product) : res.status(404).json({ error: 'Item not found' });
     };
 
-    addProduct = async ({ body }, res) => {
+    async addProduct ({ body }, res) {
         const newProduct = await producto.save(body);
         Object.keys(newProduct).length > 0 ?
             res.json({ message: 'Product has been added', data: newProduct }) :
             res.status(400).json({ error: 'Item not created' });
     };
 
-    updateProduct = async (req, res) => {
+    async updateProduct (req, res) {
         const result = await producto.updateById(parseInt(req.params.id), req.body);
         Object.keys(result).length > 0 ?
             res.json({ message: 'Product has been updated', data: result }) :
             res.status(404).json({ error: 'Item not found' });
     };
 
-    deleteProduct = async (req, res) => {
+    async deleteProduct (req, res) {
         const result = await producto.deleteById(parseInt(req.params.id));
         result.length > 0 ? res.json({ message: 'Product has been deleted' }) : res.status(404).json({ error: 'Item not found' });
     };
